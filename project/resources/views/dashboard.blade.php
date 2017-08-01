3741829812c3bf380e734a27314f6b551e022d92
evonsiow <evonmiyako.em@gmail.com>
2017-08-01 15:42:02 +0800

    chg

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -4,9 +4,9 @@
 
 <link href="{{ asset('/css/default.css') }}" rel="stylesheet">
 	<div class = "container">
-			<h4> {!!Auth::user()->firstName!!} {!!Auth::user()->lastName!!}</h4> 
-			<h4> Member since {!!Auth::user()->created_at!!}</h4>
-			<img src = "/upload/{!!Auth::user()->image!!}"/>
+		<img src = "/upload/{!!Auth::user()->image!!}" alt="profilepic" class="img-rounded center-block img-responsive" height="200" width="100"/>
+		<h4> {!!Auth::user()->firstName!!} {!!Auth::user()->lastName!!}</h4> 
+		<h4> Member since {!!Auth::user()->created_at!!}</h4>
 	</div>
 @include('footer')
 @stop
